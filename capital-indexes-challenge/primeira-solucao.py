863a38b785f79cb55e226c9313f221857e4286d6
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-08-10 12:00:25 -0300

    Corrigindo primeiro desafio

--- capital-indexes-challenge/primeira-solucao.py
+++ capital-indexes-challenge/primeira-solucao.py
@@ -2,10 +2,14 @@
 
 def capital_indexes(word):
     indexes = []
+    index_counter = 0
     for character in word:
         if character.isupper():
-            indexes.append(word.index(character))
+            print(index_counter)
+            indexes.append(index_counter)
+        index_counter += 1
     return indexes
 
 
 print(f"Deve retornar 0, 2, 4 | {capital_indexes('HeLlO') == [0, 2, 4]}")
+print(f"Deve retornar 0, 1, 3 | {capital_indexes('TEsT') == [0, 1, 3]}")
--- capital-indexes-challenge/solucao-melhorada.py
+++ capital-indexes-challenge/solucao-melhorada.py
@@ -4,3 +4,5 @@ def capital_indexes(word):
 
 
 print(f"Deve retornar 0, 2, 4 | {capital_indexes('HeLlO') == [0, 2, 4]}")
+print(f"Deve retornar 0, 1, 3 | {capital_indexes('TEsT') == [0, 1, 3]}")
+
