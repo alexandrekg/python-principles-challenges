f5b31877e479e5deffe9d81879bb7a0fcae8aca9
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-08-10 12:05:00 -0300

    Corrigindo versão melhorada

--- capital-indexes-challenge/solucao-melhorada.py
+++ capital-indexes-challenge/solucao-melhorada.py
@@ -1,8 +1,7 @@
 
 def capital_indexes(word):
-    return [word.index(character) for character in word if character.isupper()]
+    return [character[0] for character in enumerate(word) if character[1].isupper()]
 
 
 print(f"Deve retornar 0, 2, 4 | {capital_indexes('HeLlO') == [0, 2, 4]}")
 print(f"Deve retornar 0, 1, 3 | {capital_indexes('TEsT') == [0, 1, 3]}")
-
